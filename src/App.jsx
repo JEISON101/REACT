b81265430ac147f2f47b3023428530bab8000f13
JEISON101 <reyesjeison2006@gmail.com>
2024-07-01 21:07:01 -0500

    reto 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ function App() {
 let [isbuttondisable, setingsbuttondisable] = useState(false)
 
   return (
-    <div className='contenedor'>
+    <>
       <button className='boton' onClick={() => setingsbuttondisable(!isbuttondisable)}>
         {isbuttondisable ? "Ocultar" : "Mostrar"}
       </button>
       {isbuttondisable? <h1 className='texto'>BIENVENIDO</h1> : null }
-    </div>
+    </>
   )
 }
 
